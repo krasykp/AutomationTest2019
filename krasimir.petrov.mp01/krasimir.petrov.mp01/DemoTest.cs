1d7b3ad64ee335f5fc19d6654dabd07a90cc2271
krasykp <58074004+krasykp@users.noreply.github.com>
2019-12-07 15:01:43 +0200

    add first version

---
+++ krasimir.petrov.mp01/krasimir.petrov.mp01/DemoTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace krasimir.petrov.mp01
+{
+    [TestClass]
+    public class DemoTest
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+        [TestMethod]
+        public void test1()
+        {
+            driver.Navigate().GoToUrl("https://edu.unibit.bg/login/index.php");
+            driver.Manage().Window.Size = new System.Drawing.Size(1045, 1039);
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).SendKeys("asdsad");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector(".alert")).Text, "Невалидно име или парола, моля опитайте отново");
+            Assert.AreEqual(driver.FindElement(By.CssSelector(".alert")).Text, "Невалидно име или парола, моля опитайте отново");
+
+
+        }
+    }
+}
---
+++ krasimir.petrov.mp01/krasimir.petrov.mp01/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("krasimir.petrov.mp01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("krasimir.petrov.mp01")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0133b6d0-bbba-42c3-8df5-600db51f6b70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
